b8bc351d27bab9470fb239c12e879bba7ede8b91
daylightInitiative <202697339+daylightInitiative@users.noreply.github.com>
2025-05-24 17:56:48 -0400

    updated main.lua

--- main.lua
+++ main.lua
@@ -1,17 +1,24 @@
 local math = require("math")
 local os = require("os")
 
+local event = require("event") -- our custom event handler
+local EventObj = event:New()
+
 -- randomize our math function
 math.randomseed(os.time())
 
 local fruits = {}
+local fruitImages = nil
 
 --io.stdout:setvbuf("no")
-local pos = {x = 300, y = 500}
+local pos = {x = 300, y = 490}
+local arrow_image = nil
 local move = {left = false, right = false}
-local basket_speed = 250
-local fruit_speed = 100
+local basket_speed = 400
+local fruit_speed = 70
 local points = 0
+
+local spread_range = {X = {-200, 200}, Y = {-100, 100}}
 
 local SCREEN_WIDTH = love.graphics.getWidth()
 local SCREEN_HEIGHT = love.graphics.getHeight()
@@ -21,13 +28,21 @@ local FRUIT_SCALE = 0.13
 local BASKET_SCALE = 0.2
 local PAUSED = false
 
+local LEVEL_COMPLETED = false
+
+local game_coroutine = nil
+
+-- the number of falling fruit remaining
+local numOfFruit = math.huge
+
 local basket_image = nil
 local basket_size = { X = 0, Y = 0 }
+local background_image = nil
+local background_offset = {X = 0, Y = 0}
 local lastSpawnTime = math.huge
 
-function love.conf(t)
-	t.console = true
-end
+local imageFileNames = nil
+
 
 function table_find(t, value)
     for i, v in ipairs(t) do
@@ -45,45 +60,17 @@ function checkCollision(x1,y1,w1,h1, x2,y2,w2,h2)
            y2 < y1 + h1
 end
 
-function love.load()
-	love.graphics.setBackgroundColor(200, 200, 200, 1)
+function create_fruit(amount, fruitImages, imageFileNames)
+	local sprX = spread_range.X
+	local sprY = spread_range.Y
 
-	-- load our basket image
-	basket_image = love.graphics.newImage("assets/basket.png")
-	-- lets resize our basket and give update its applied scale
-	basket_size.X = basket_image:getWidth() * BASKET_SCALE
-	basket_size.Y = basket_image:getHeight() * BASKET_SCALE
-
-	-- set our font
-	myFont = love.graphics.newFont(24)
-    love.graphics.setFont(myFont)
-
-    -- table to store images by filename
-    fruitImages = {}
-
-    -- load all fruit images
-    local imageFiles = love.filesystem.getDirectoryItems("assets/fruit_images")
-
-    -- filter image files and load images
-    local imageFileNames = {}  -- to hold just the valid filenames
-    for _, filename in ipairs(imageFiles) do
-        if filename:match("%.png$") or filename:match("%.jpg$") or filename:match("%.jpeg$") then
-            local path = "assets/fruit_images/" .. filename
-            fruitImages[filename] = love.graphics.newImage(path)
-            table.insert(imageFileNames, filename)  
-        end
-    end
-
-    fruits = {}
-
-    -- create 5 fruit objects
-    for i = 1, 5 do
+	for i = 1, amount do
         local randomIndex = math.random(1, #imageFileNames)
         local chosenFilename = imageFileNames[randomIndex]
 
         local randomPos = {
-            randX = math.random(50, 500),
-            randY = math.random(50, 250),
+            randX = math.random(50, 500) + math.random(sprX[1], sprX[2]),
+            randY = math.random(5, 250) + math.random(sprY[1], sprY[2]),
         }
 
         local fruitObject = {
@@ -103,8 +90,177 @@ function love.load()
     end
 end
 
+-- timer switches
+local levelCompleteTimerStarted = false
+local shouldShowArrows = false
+
+local timers = {}
+
+function register_timer(delay, callback, repeats, ...)
+    table.insert(timers, {
+        callback = callback,
+        delayTime = delay,
+        callback_args = {...},
+        --_doesTimerRepeat = repeats,
+        _delayTimer = 0
+    })
+end
+
+function gameLevelLogic()
+	-- lets start level 2
+			-- numOfFruit = 10
+			-- create_fruit(numOfFruit, fruitImages, imageFileNames)
+
+			-- register_timer(3, function()
+
+			-- 	numOfFruit = 10
+			-- 	create_fruit(numOfFruit, fruitImages, imageFileNames)
+				
+
+			-- end)
+
+			-- register_timer(3, function()
+
+			-- 	numOfFruit = 10
+			-- 	create_fruit(numOfFruit, fruitImages, imageFileNames)
+				
+
+			-- end)
+	while true do
+        print("Waiting for LevelCompleted event...")
+        event.waitUntil(EventObj, "LevelCompleted")
+        print("Event fired, coroutine resumed!")
+
+		--levelCompleteTimerStarted = false
+
+
+        numOfFruit = 10
+        create_fruit(numOfFruit, fruitImages, imageFileNames)
+
+        --levelCompleteTimerStarted = false
+    end
+end
+
+function love.load()
+	love.graphics.setBackgroundColor(200, 200, 200, 1)
+
+	-- load our backgrond image
+	background_image = love.graphics.newImage("assets/fruitgame_background.png")
+
+	local scaleX = background_image:getWidth()
+	local scaleY = background_image:getHeight()
+
+	background_offset.scaleX = SCREEN_WIDTH / scaleX
+	background_offset.scaleY = SCREEN_HEIGHT / scaleY
+
+	-- load our arrow image (shared we just need to draw multiple times)
+	arrow_image = love.graphics.newImage("assets/arrow.png")
+
+	-- load our basket image
+	basket_image = love.graphics.newImage("assets/basket.png")
+	-- lets resize our basket and give update its applied scale
+	basket_size.X = basket_image:getWidth() * BASKET_SCALE
+	basket_size.Y = basket_image:getHeight() * BASKET_SCALE
+
+	-- set our font
+	myFont = love.graphics.newFont(24)
+    love.graphics.setFont(myFont)
+
+    -- table to store images by filename
+    fruitImages = {}
+
+    -- load all fruit images
+    local imageFiles = love.filesystem.getDirectoryItems("assets/fruit_images")
+
+    -- filter image files and load images
+    imageFileNames = {}  -- to hold just the valid filenames
+    for _, filename in ipairs(imageFiles) do
+        if filename:match("%.png$") or filename:match("%.jpg$") or filename:match("%.jpeg$") then
+            local path = "assets/fruit_images/" .. filename
+            fruitImages[filename] = love.graphics.newImage(path)
+            table.insert(imageFileNames, filename)  
+        end
+    end
+
+    fruits = {}
+
+    -- create 5 fruit objects
+	numOfFruit = 5
+    create_fruit(numOfFruit, fruitImages, imageFileNames)
+
+	-- create the level listener
+	EventObj:OnEvent("LevelCompleted", function()
+
+		print("Level completed!")
+		
+	end)
+
+	-- create our game logic coroutine
+	game_coroutine = coroutine.create(gameLevelLogic)
+
+	local ok, err = coroutine.resume(game_coroutine)
+	if not ok then
+		print("Error in coroutine:", err)
+	end
+end
+
+function newLevelArrowEffect()
+
+	-- since the time system is non yielding we need to offset the delay
+	-- using the index
+	local onDuration = 0.15        -- how long arrows stay on
+	local offDuration = 0.17       -- how long arrows stay off
+	local totalCycle = onDuration + offDuration
+
+	for i = 0, 3 do
+		local onDelay = i * totalCycle
+		local offDelay = onDelay + onDuration
+
+		register_timer(onDelay, function()
+			--print("toggle on")
+			shouldShowArrows = true
+		end)
+
+		register_timer(offDelay, function()
+			--print("toggle off")
+			shouldShowArrows = false
+		end)
+	end
+end
+
 function love.update(dt)
 	if PAUSED then return end
+
+	if numOfFruit == 0 and not levelCompleteTimerStarted then
+		levelCompleteTimerStarted = true
+		register_timer(3, function()
+			print("Timer has completed!")
+
+			newLevelArrowEffect()
+			print("we have passed the effect")
+
+			EventObj:Fire("LevelCompleted")
+		end)
+	end
+
+	-- if we do it this way this supports multiple timers per callback
+	for i = #timers, 1, -1 do
+        local timer = timers[i]
+        timer._delayTimer = timer._delayTimer + dt
+
+        if timer._delayTimer >= timer.delayTime then
+            timer.callback(unpack(timer.callback_args))
+
+			-- removing repeat logic for now
+            -- if timer._doesTimerRepeat == true then
+            --     timer._delayTimer = 0
+            -- else
+            --     table.remove(timers, i)
+            -- end
+			table.remove(timers, i)
+        end
+    end
+
 	if move.left then
 		pos.x = pos.x - basket_speed * dt
 	end
@@ -135,11 +291,13 @@ function love.update(dt)
 		) and fruit_obj.IsMoving == true then
 			print("we caught a fruit!")
 			points = points + 5
+			numOfFruit = numOfFruit - 1
 			table.remove(fruits, i)
 
 		elseif fruit_obj.Position.Y > SCREEN_HEIGHT then
 
 			print("missed a fruit!")
+			numOfFruit = numOfFruit - 1
 			table.remove(fruits, i)
 		end
 	end
@@ -169,16 +327,31 @@ function love.keyreleased(key)
 	end
 end
 
-local renderlist = {}
 
 function love.draw()
 
+	-- -- load the background image
+	-- love.graphics.draw(background_image, 0, 0)
+	love.graphics.draw(background_image, 0, 0, 0,                             -- no rotation
+    	background_offset.scaleX,
+    	background_offset.scaleY
+	)
+
+
 	if PAUSED == true then
+		-- create paused bg
+		love.graphics.setColor(200, 200, 200, 1) -- red, semi-transparent
+		love.graphics.rectangle("fill", (SCREEN_HEIGHT/2), (SCREEN_HEIGHT/2)-25, 200, 100-25)
+		love.graphics.setColor(1, 1, 1, 1) -- reset color
+
 		love.graphics.setColor(0, 0, 0, 1)
 		love.graphics.printf("PAUSED", 0, SCREEN_HEIGHT/2, SCREEN_WIDTH, "center")
-		love.graphics.setColor(1, 1, 1, 1) -- reset color
+
+		
+		--return
 	end
 
+	
 	love.graphics.setColor(0, 0, 0, 1)
 	local points_str = string.format("Points: %d", points)
     love.graphics.printf(points_str, 0, 10, SCREEN_WIDTH, "left")
@@ -190,6 +363,22 @@ function love.draw()
 	love.graphics.setColor(1, 0, 0, 0.3) -- red, semi-transparent
 	love.graphics.rectangle("fill", pos.x, pos.y+35, basket_size.X, basket_size.Y-35)
 	love.graphics.setColor(1, 1, 1, 1) -- reset color
+
+	-- lets draw our arrows
+	if shouldShowArrows == true then
+		local tempPos = 125
+		for i = 1, 4 do
+
+			if tempPos == 2 then
+				tempPos = tempPos + 400
+			end
+
+			love.graphics.draw(arrow_image, tempPos, 80, 0, 0.5, 0.5)
+			tempPos = tempPos + 150
+
+		end
+	end
+
 
 	for i, fruit_obj in ipairs(fruits) do
 		
